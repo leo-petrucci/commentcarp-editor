2cde76a8870442409f54858b4e33165133e130f4
Leonardo Petrucci <leonardo@ironeko.com>
2021-10-03 17:07:31 +0100

    ⚗️ Undo alpine update

--- src/main.ts
+++ src/main.ts
@@ -2,37 +2,22 @@
 import Alpine from "alpinejs";
 // @ts-ignore
 import html from "html-template-string";
-// @ts-ignore
 import { Content, Editor } from "@tiptap/core";
-// @ts-ignore
 import Document from "@tiptap/extension-document";
-// @ts-ignore
 import Paragraph from "@tiptap/extension-paragraph";
-// @ts-ignore
 import BulletList from "@tiptap/extension-bullet-list";
-// @ts-ignore
 import ListItem from "@tiptap/extension-list-item";
-// @ts-ignore
 import Text from "@tiptap/extension-text";
-// @ts-ignore
 import Bold from "@tiptap/extension-bold";
-// @ts-ignore
 import Italic from "@tiptap/extension-italic";
-// @ts-ignore
 import Code from "@tiptap/extension-code";
-// @ts-ignore
 import Mention from "@tiptap/extension-mention";
-// @ts-ignore
 import CodeBlock from "@tiptap/extension-code-block";
-// @ts-ignore
 import Blockquote from "@tiptap/extension-blockquote";
-// @ts-ignore
 import Placeholder from "@tiptap/extension-placeholder";
 // @ts-ignore
 import styles from "./assets/main.css";
-// @ts-ignore
 import tippy from "tippy.js";
-// @ts-ignoree
 import { SuggestionProps } from "@tiptap/suggestion";
 
 const script = document.querySelector('script[data-name="commentcarp"]');
@@ -58,8 +43,7 @@ const init = async () => {
     commentcarpRoot.appendChild(template.content!);
     const comp = commentcarpRoot.querySelector("[defer-x-data]")!;
     comp.setAttribute("x-data", comp.getAttribute("defer-x-data")!);
-
-    Alpine.start();
+    Alpine.initializeComponent(comp);
   }
 };
 
@@ -69,39 +53,36 @@ window.addEventListener("initCommentCarp", function () {
   init();
 });
 
-// declare global {
-//   interface Window {
-//     comment: (content: Content) => {
-//       editor: Editor | null;
-//       loading: boolean;
-//       loggedIn?: boolean;
-//       user: ConvertedUserInterface | null;
-//       comments: {
-//         isError: boolean;
-//         isLoading: boolean;
-//         list: CommentsInterface[];
-//       };
-//       getComments: () => Promise<unknown>;
-//       init: (element: Element) => void;
-//       post: () => void;
-//       checkLogin: () => void;
-//     };
-//   }
-// }
+declare global {
+  interface Window {
+    comment: (content: Content) => {
+      editor: Editor | null;
+      loading: boolean;
+      loggedIn?: boolean;
+      user: ConvertedUserInterface | null;
+      comments: {
+        isError: boolean;
+        isLoading: boolean;
+        list: CommentsInterface[];
+      };
+      getComments: () => Promise<unknown>;
+      init: (element: Element) => void;
+      post: () => void;
+      checkLogin: () => void;
+    };
+  }
+}
 
 const endpoint = import.meta.env.VITE_API_URL;
 
-document.addEventListener("alpine:init", () => {
-  Alpine.data("comment", () => comment);
-});
-
-const comment = () => {
+const comment = (content: Content = "") => {
   return {
     loading: true,
     loggedIn: undefined,
     user: null,
 
     editor: null as null | Editor,
+    content: content,
     errorMessage: "",
 
     comments: {
@@ -111,234 +92,210 @@ const comment = () => {
     },
 
     async init(element: Element) {
+      const { getAllCommenters } = await fetchCommenters();
+
       this.editor = new Editor({
         element: element,
-        extensions: [],
+        extensions: [
+          Document,
+          Paragraph,
+          Text,
+          Bold,
+          Italic,
+          BulletList,
+          ListItem,
+          Code,
+          CodeBlock,
+          Blockquote,
+          Placeholder.configure({ placeholder: "Write a comment!" }),
+          Mention.configure({
+            HTMLAttributes: {
+              class: "mention",
+            },
+            suggestion: {
+              items: (query) => {
+                return getAllCommenters
+                  .map(({ username }) => username)
+                  .filter((item) =>
+                    item.toLowerCase().startsWith(query.toLowerCase())
+                  )
+                  .slice(0, 5);
+              },
+              render: () => {
+                let popup: any;
+
+                const selectItem = (props: SuggestionProps, item: any) => {
+                  if (item) {
+                    props.command({ id: item, mention: "idk" });
+                  }
+                };
+
+                const menu = (props: SuggestionProps) => {
+                  const div = document.createElement("div");
+                  const items = document.createElement("div");
+                  items.className = "items";
+
+                  props.items.forEach((suggestion) => {
+                    const button = document.createElement("button");
+                    button.innerText = suggestion;
+                    button.className = "item";
+                    button.addEventListener("click", function () {
+                      selectItem(props, suggestion);
+                    });
+                    items.appendChild(button);
+                  });
+
+                  div.appendChild(items);
+                  return div.firstChild;
+                };
+                return {
+                  onStart: (props) => {
+                    // @ts-ignore
+                    popup = tippy("body", {
+                      getReferenceClientRect: props.clientRect,
+                      appendTo: () =>
+                        document.getElementById("commentcarp") as Element,
+                      content: menu(props),
+                      showOnCreate: true,
+                      interactive: true,
+                      allowHTML: true,
+                      trigger: "manual",
+                      placement: "bottom-start",
+                    });
+
+                    console.log(popup[0]);
+                  },
+                  onUpdate(props) {
+                    popup[0].setProps({
+                      getReferenceClientRect: props.clientRect,
+                      content: menu(props),
+                    });
+                  },
+                  onKeyDown() {
+                    return false;
+                  },
+                  onExit() {
+                    popup[0].destroy();
+                  },
+                };
+              },
+            },
+          }),
+        ],
+        content: this.content,
+        onUpdate: ({ editor }) => {
+          this.content = editor.getHTML();
+        },
       });
+    },
+    addListener() {
+      window.addEventListener(
+        "message",
+        (event) => {
+          if (event.origin === endpoint) {
+            const cookie = `token=${window.escape(
+              event.data
+            )}; Max-Age=604800;`;
+            document.cookie = cookie;
+            this.checkLogin();
+          }
+        },
+        false
+      );
+    },
+    async getComments() {
+      try {
+        const { getAllComments } = await fetchComments();
+        // @ts-ignore
+        this.comments.isLoading = false;
+        // @ts-ignore
+        this.comments.list = getAllComments;
+        // @ts-ignore
+      } catch (err) {
+        // @ts-ignore
+        this.comments.isLoading = false;
+      }
+    },
+    getLink(user: ConvertedUserInterface) {
+      switch (user.provider) {
+        case "twitter":
+          return `https://twitter.com/i/user/${user.platformId}`;
+      }
+    },
+    async checkLogin() {
+      const user = await auth();
+      if (user.getMyCommenterProfile) {
+        // @ts-ignore
+        this.user = user.getMyCommenterProfile;
+        this.loading = false;
+        // @ts-ignore
+        this.loggedIn = true;
+      } else {
+        this.loading = false;
+        // @ts-ignore
+        this.loggedIn = false;
+      }
+    },
+    async post() {
+      if (this.loggedIn) {
+        this.loading = true;
+        try {
+          await send(this.content as string);
+          this.loading = false;
+          await this.getComments();
+          this.editor?.commands.clearContent();
+          this.content = "";
+        } catch (err) {
+          this.loading = false;
+          this.errorMessage = err.toString().replace("Error:", "");
+          throw new Error(err);
+        }
+      } else {
+        this.login();
+      }
+    },
+    login() {
+      const w = 450;
+      const h = 450;
+      const dualScreenLeft =
+        window.screenLeft !== undefined ? window.screenLeft : window.screenX;
+      const dualScreenTop =
+        window.screenTop !== undefined ? window.screenTop : window.screenY;
+
+      const width = window.innerWidth
+        ? window.innerWidth
+        : document.documentElement.clientWidth
+        ? document.documentElement.clientWidth
+        : screen.width;
+      const height = window.innerHeight
+        ? window.innerHeight
+        : document.documentElement.clientHeight
+        ? document.documentElement.clientHeight
+        : screen.height;
+
+      const systemZoom = width / window.screen.availWidth;
+      const left = (width - w) / 2 / systemZoom + dualScreenLeft;
+      const top = (height - h) / 2 / systemZoom + dualScreenTop;
+      window.open(
+        `${endpoint}/api/oauth/`,
+        "Twitter Login",
+        `
+        height=${h},
+        width=${w}, 
+        top=${top}, 
+        left=${left}
+      `
+      );
     },
   };
 };
 
-// const comment = (content: Content = "") => {
-//   return {
-//     loading: true,
-//     loggedIn: undefined,
-//     user: null,
+window.comment = comment;
 
-//     editor: null as null | Editor,
-//     content: content,
-//     errorMessage: "",
-
-//     comments: {
-//       isLoading: true,
-//       isError: false,
-//       list: [],
-//     },
-
-//     async init(element: Element) {
-//       const { getAllCommenters } = await fetchCommenters();
-
-//       this.editor = new Editor({
-//         element: element,
-//         extensions: [
-//           Document,
-//           Paragraph,
-//           Text,
-//           Bold,
-//           Italic,
-//           BulletList,
-//           ListItem,
-//           Code,
-//           CodeBlock,
-//           Blockquote,
-//           Placeholder.configure({ placeholder: "Write a comment!" }),
-//           Mention.configure({
-//             HTMLAttributes: {
-//               class: "mention",
-//             },
-//             suggestion: {
-//               items: (query) => {
-//                 return getAllCommenters
-//                   .map(({ username }) => username)
-//                   .filter((item) =>
-//                     item.toLowerCase().startsWith(query.toLowerCase())
-//                   )
-//                   .slice(0, 5);
-//               },
-//               render: () => {
-//                 let popup: any;
-
-//                 const selectItem = (props: SuggestionProps, item: any) => {
-//                   if (item) {
-//                     props.command({ id: item, mention: "idk" });
-//                   }
-//                 };
-
-//                 const menu = (props: SuggestionProps) => {
-//                   const div = document.createElement("div");
-//                   const items = document.createElement("div");
-//                   items.className = "items";
-
-//                   props.items.forEach((suggestion) => {
-//                     const button = document.createElement("button");
-//                     button.innerText = suggestion;
-//                     button.className = "item";
-//                     button.addEventListener("click", function () {
-//                       selectItem(props, suggestion);
-//                     });
-//                     items.appendChild(button);
-//                   });
-
-//                   div.appendChild(items);
-//                   return div.firstChild;
-//                 };
-//                 return {
-//                   onStart: (props) => {
-//                     // @ts-ignore
-//                     popup = tippy("body", {
-//                       getReferenceClientRect: props.clientRect,
-//                       appendTo: () =>
-//                         document.getElementById("commentcarp") as Element,
-//                       content: menu(props),
-//                       showOnCreate: true,
-//                       interactive: true,
-//                       allowHTML: true,
-//                       trigger: "manual",
-//                       placement: "bottom-start",
-//                     });
-
-//                     console.log(popup[0]);
-//                   },
-//                   onUpdate(props) {
-//                     popup[0].setProps({
-//                       getReferenceClientRect: props.clientRect,
-//                       content: menu(props),
-//                     });
-//                   },
-//                   onKeyDown() {
-//                     return false;
-//                   },
-//                   onExit() {
-//                     popup[0].destroy();
-//                   },
-//                 };
-//               },
-//             },
-//           }),
-//         ],
-//         content: this.content,
-//         onUpdate: ({ editor }) => {
-//           this.content = editor.getHTML();
-//         },
-//       });
-//     },
-//     addListener() {
-//       window.addEventListener(
-//         "message",
-//         (event) => {
-//           if (event.origin === endpoint) {
-//             const cookie = `token=${window.escape(
-//               event.data
-//             )}; Max-Age=604800;`;
-//             document.cookie = cookie;
-//             this.checkLogin();
-//           }
-//         },
-//         false
-//       );
-//     },
-//     async getComments() {
-//       try {
-//         const { getAllComments } = await fetchComments();
-//         // @ts-ignore
-//         this.comments.isLoading = false;
-//         // @ts-ignore
-//         this.comments.list = getAllComments;
-//         // @ts-ignore
-//       } catch (err) {
-//         // @ts-ignore
-//         this.comments.isLoading = false;
-//       }
-//     },
-//     getLink(user: ConvertedUserInterface) {
-//       switch (user.provider) {
-//         case "twitter":
-//           return `https://twitter.com/i/user/${user.platformId}`;
-//       }
-//     },
-//     async checkLogin() {
-//       const user = await auth();
-//       if (user.getMyCommenterProfile) {
-//         // @ts-ignore
-//         this.user = user.getMyCommenterProfile;
-//         this.loading = false;
-//         // @ts-ignore
-//         this.loggedIn = true;
-//       } else {
-//         this.loading = false;
-//         // @ts-ignore
-//         this.loggedIn = false;
-//       }
-//     },
-//     async post() {
-//       if (this.loggedIn) {
-//         this.loading = true;
-//         try {
-//           await send(this.content as string);
-//           this.loading = false;
-//           await this.getComments();
-//           this.editor?.commands.clearContent();
-//           this.content = "";
-//         } catch (err: any) {
-//           this.loading = false;
-//           this.errorMessage = err.toString().replace("Error:", "");
-//           throw new Error(err);
-//         }
-//       } else {
-//         this.login();
-//       }
-//     },
-//     login() {
-//       const w = 450;
-//       const h = 450;
-//       const dualScreenLeft =
-//         window.screenLeft !== undefined ? window.screenLeft : window.screenX;
-//       const dualScreenTop =
-//         window.screenTop !== undefined ? window.screenTop : window.screenY;
-
-//       const width = window.innerWidth
-//         ? window.innerWidth
-//         : document.documentElement.clientWidth
-//         ? document.documentElement.clientWidth
-//         : screen.width;
-//       const height = window.innerHeight
-//         ? window.innerHeight
-//         : document.documentElement.clientHeight
-//         ? document.documentElement.clientHeight
-//         : screen.height;
-
-//       const systemZoom = width / window.screen.availWidth;
-//       const left = (width - w) / 2 / systemZoom + dualScreenLeft;
-//       const top = (height - h) / 2 / systemZoom + dualScreenTop;
-//       window.open(
-//         `${endpoint}/api/oauth/`,
-//         "Twitter Login",
-//         `
-//         height=${h},
-//         width=${w},
-//         top=${top},
-//         left=${left}
-//       `
-//       );
-//     },
-//   };
-// };
-
-interface ConvertedUserInterface extends CommenterInterface {
+export interface ConvertedUserInterface extends CommenterInterface {
   verified?: boolean;
 }
 
-// @ts-ignore
 const auth = async (): Promise<{
   getMyCommenterProfile?: ConvertedUserInterface;
 }> => {
@@ -370,7 +327,6 @@ interface CommentsInterface {
   body: string;
 }
 
-// @ts-ignore
 const fetchComments = async (): Promise<{
   getAllComments: CommentsInterface[];
 }> => {
@@ -396,13 +352,12 @@ const fetchComments = async (): Promise<{
   }
 };
 
-interface CommentResponseInterface {
+export interface CommentResponseInterface {
   origin: string;
   commenter: CommenterInterface;
   body: string;
 }
 
-// @ts-ignore
 const send = async (
   comment: string
 ): Promise<{ comment?: CommentResponseInterface }> => {
@@ -429,7 +384,7 @@ const send = async (
   }
 };
 
-interface CommenterInterface {
+export interface CommenterInterface {
   platformId: string;
   provider: "twitter";
   username: string;
@@ -437,7 +392,6 @@ interface CommenterInterface {
   photo: string;
 }
 
-// @ts-ignore
 const fetchCommenters = async (): Promise<{
   getAllCommenters: CommenterInterface[];
 }> => {
