79f6b7a39b80a988178610a3aacd8d6dd1bd2689
Leonardo Petrucci <leonardo@ironeko.com>
2021-10-04 20:22:25 +0100

    💄 Dark mode settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,17 @@ const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
+  darkMode: "class",
   prefix: "cc-",
   purge: {
     enabled: process.env.NODE_ENV === "production",
-    // classes that are generated dynamically, e.g. `rounded-${size}` and must
-    // be kept
     safeList: [],
-    content: [
-      "./index.html",
-      "./template.html",
-      "./src/**/*.vue",
-      "./src/**/*.js",
-      // etc.
-    ],
+    content: ["./index.html", "./template.html", "./src/**/*.js"],
   },
   variants: {
     extend: {
       opacity: ["disabled"],
+      typography: ["dark"],
     },
   },
   theme: {
@@ -30,28 +24,59 @@ module.exports = {
       colors: {
         orange: colors.orange,
       },
-      typography: {
-        DEFAULT: {
-          css: {
-            "code::before": false,
-            "code::after": false,
-            "blockquote p:first-of-type::before": false,
-            "blockquote p:first-of-type::after": false,
-            "ul li p": false,
-            p: {
-              margin: ".5rem 0",
-            },
-            pre: {
-              margin: ".5rem 0",
-            },
-            ul: {
-              margin: ".5rem 0",
-            },
-            blockquote: {
-              margin: ".5rem 0",
+      typography(theme) {
+        return {
+          dark: {
+            css: {
+              color: theme("colors.gray.300"),
+              '[class~="lead"]': { color: theme("colors.gray.400") },
+              a: { color: theme("colors.gray.100") },
+              strong: { color: theme("colors.gray.100") },
+              "ul > li::before": { backgroundColor: theme("colors.gray.700") },
+              hr: { borderColor: theme("colors.gray.800") },
+              blockquote: {
+                color: theme("colors.gray.100"),
+                borderLeftColor: theme("colors.gray.800"),
+              },
+              h1: { color: theme("colors.gray.100") },
+              h2: { color: theme("colors.gray.100") },
+              h3: { color: theme("colors.gray.100") },
+              h4: { color: theme("colors.gray.100") },
+              code: { color: theme("colors.gray.100") },
+              "a code": { color: theme("colors.gray.100") },
+              pre: {
+                color: theme("colors.gray.200"),
+                backgroundColor: theme("colors.gray.800"),
+              },
+              thead: {
+                color: theme("colors.gray.100"),
+                borderBottomColor: theme("colors.gray.700"),
+              },
+              "tbody tr": { borderBottomColor: theme("colors.gray.800") },
             },
           },
-        },
+          DEFAULT: {
+            css: {
+              "code::before": false,
+              "code::after": false,
+              "blockquote p:first-of-type::before": false,
+              "blockquote p:first-of-type::after": false,
+              "ul li p": false,
+              p: {
+                margin: ".5rem 0",
+              },
+              pre: {
+                margin: ".5rem 0",
+              },
+              ul: {
+                margin: ".5rem 0",
+              },
+              blockquote: {
+                margin: ".5rem 0",
+              },
+            },
+          },
+        };
       },
     },
   },
