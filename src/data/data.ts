b4a703a3a7cc9dfcbdc32c0322b5fac709c1172c
Leonardo Petrucci <leonardo@creativiii.com>
2022-07-17 10:37:56 +0100

    ✨ Introduce better mentions

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,4 +1,4 @@
-import { endpoint, key } from "../main";
+import { endpoint, key } from '../main';
 
 export interface ConvertedUserInterface extends CommenterInterface {
   verified?: boolean;
@@ -7,19 +7,15 @@ export interface ConvertedUserInterface extends CommenterInterface {
 export const auth = async (): Promise<{
   getMyCommenterProfile?: ConvertedUserInterface;
 }> => {
-  const headers = new Headers();
-  headers.append("Content-Type", "application/json");
-
   const body = JSON.stringify({
     query:
-      "query {\r\n    getMyCommenterProfile {\r\n        username\r\n        displayName\r\n        photo\r\n    }\r\n}",
+      'query {\r\n    getMyCommenterProfile {\r\n        username\r\n        displayName\r\n        photo\r\n    }\r\n}',
     variables: {},
   });
   try {
     return (await handleGraphQL({
-      headers,
       body,
-      identifier: "?getMyCommenterProfile",
+      identifier: '?getMyCommenterProfile',
     })) as {
       getMyCommenterProfile: ConvertedUserInterface;
     };
@@ -38,19 +34,15 @@ export interface CommentsInterface {
 export const fetchComments = async (): Promise<{
   getAllComments: CommentsInterface[];
 }> => {
-  const headers = new Headers();
-  headers.append("Content-Type", "application/json");
-
   const body = JSON.stringify({
     query:
-      "query ($origin: String!, $key: String!) {\r\n    getAllComments (origin: $origin, key: $key) {\r\n        id\r\n        origin\r\n        commenter\r\n        { platformId\r\n        provider\r\n        displayName\r\n        username\r\n        photo\r\n }       body\r\n    }\r\n}",
+      'query ($origin: String!, $key: String!) {\r\n    getAllComments (origin: $origin, key: $key) {\r\n        id\r\n        origin\r\n        commenter\r\n        { platformId\r\n        provider\r\n        displayName\r\n        username\r\n        photo\r\n }       body\r\n    }\r\n}',
     variables: { origin: window.location.href, key },
   });
   try {
     return (await handleGraphQL({
-      headers,
       body,
-      identifier: "?getAllComments",
+      identifier: '?getAllComments',
     })) as {
       getAllComments: CommentsInterface[];
     };
@@ -69,20 +61,16 @@ export interface CommentResponseInterface {
 export const send = async (
   comment: string
 ): Promise<{ comment?: CommentResponseInterface }> => {
-  const headers = new Headers();
-  headers.append("Content-Type", "application/json");
-
   const body = JSON.stringify({
     query:
-      "mutation ($body: String!, $origin: String!, $key: String!) {\r\n    addOneComment (body: $body, origin: $origin, key: $key) {\r\n        body\r\n        origin\r\n                commenter\r\n        { username\r\n }    }\r\n}",
+      'mutation ($body: String!, $origin: String!, $key: String!) {\r\n    addOneComment (body: $body, origin: $origin, key: $key) {\r\n        body\r\n        origin\r\n                commenter\r\n        { username\r\n }    }\r\n}',
     variables: { origin: window.location.href, body: comment, key },
   });
 
   try {
     return (await handleGraphQL({
-      headers,
       body,
-      identifier: "?addOneComment",
+      identifier: '?addOneComment',
     })) as {
       comment: CommentResponseInterface;
     };
@@ -93,8 +81,9 @@ export const send = async (
 };
 
 export interface CommenterInterface {
+  id: string;
   platformId: string;
-  provider: "twitter";
+  provider: 'twitter';
   username: string;
   displayName: string;
   photo: string;
@@ -103,13 +92,11 @@ export interface CommenterInterface {
 export const fetchCommenters = async (): Promise<{
   getAllCommenters: CommenterInterface[];
 }> => {
-  const headers = new Headers();
-  headers.append("Content-Type", "application/json");
-
   const body = JSON.stringify({
     query: `
         query ($origin: String!, $key: String!) {
           getAllCommenters(origin: $origin, key: $key) {
+            id
             platformId
             provider
             displayName
@@ -122,9 +109,8 @@ export const fetchCommenters = async (): Promise<{
   });
   try {
     return (await handleGraphQL({
-      headers,
       body,
-      identifier: "?getAllCommenters",
+      identifier: '?getAllCommenters',
     })) as {
       getAllCommenters: CommenterInterface[];
     };
@@ -137,30 +123,28 @@ export const fetchCommenters = async (): Promise<{
 function getCookie(name: string): string | null {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop()?.split(";").shift()!;
+  if (parts.length === 2) return parts.pop()?.split(';').shift()!;
   return null;
 }
 
 const handleGraphQL = async ({
-  headers,
   body,
   identifier,
 }: {
-  headers: Headers;
   body: string;
   identifier?: string;
 }): Promise<unknown> => {
-  let parsedHeaders = headers;
+  const headers = new Headers();
+  headers.append('Content-Type', 'application/json');
 
-  const token = getCookie("token");
-  if (token) parsedHeaders.append("Authorization", `Bearer ${token}`);
+  const token = getCookie('token');
+  if (token) headers.append('Authorization', `Bearer ${token}`);
 
-  const result = await fetch(`${endpoint!}/api${identifier || ""}`, {
-    method: "POST",
-    headers: parsedHeaders,
+  const result = await fetch(`${endpoint!}/api${identifier || ''}`, {
+    method: 'POST',
+    headers,
     body,
-    redirect: "follow",
-  }).then((response) => response.json());
+  }).then(response => response.json());
 
   if (result.errors) {
     throw new Error(result.errors[0].message);
